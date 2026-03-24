9beb29c3d7fdd0b0b91cbf756f40c62a93e385ad
Eraldo Pereira <contato.eraldopereira@gmail.com>
2026-03-24 00:13:08 -0300

    Criado BD local, convertido BD do SQLite para Postgres, Instalado servico do PG, acessado via Dbeaver, criado estrutura de INI

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,27 @@
-const Database = require("better-sqlite3")
+/*const Database = require("better-sqlite3")
 const path = require("path")
 
 // Caminho para o banco já existente
 const dbPath = path.join(__dirname, "../db/Ondebit.db")
 const db = new Database(dbPath)
 
-module.exports = db
+module.exports = db */
+
+const fs = require("fs")
+const ini = require("ini")
+const { Pool } = require("pg")
+const path = require("path")
+
+const configPath = path.join(__dirname, "..", "ONDEBIT.INI")
+
+const config = ini.parse(fs.readFileSync(configPath, "utf-8"))
+
+const pool = new Pool({
+  host: config.database.host,
+  port: config.database.port,
+  database: config.database.database,
+  user: config.database.user,
+  password: config.database.password,
+})
+
+module.exports = pool
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 const express = require("express")
 const cors = require("cors")
 
+const db = require("./db")
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.send("API Ondebit rodando 🚀")
+}) */
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT NOW()")
+
+    res.json(result.rows)
+  } catch (err) {
+    console.error("Erro conexão banco:", err)
+    res.status(500).send("Erro conexão banco")
+  }
 })
 
 app.listen(3000, () => {
